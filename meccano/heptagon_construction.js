7a9eaf8da8e12bced3357a380879610fe3838e7c
Jorge Mireles <jorgeluismireles@gmail.com>
2019-09-20 13:49:20 -0500

    build meccano heptagon bars 3 and 4

--- meccano/heptagon_construction.js
+++ meccano/heptagon_construction.js
@@ -20,15 +20,34 @@ const xml = (tag, attrs, text)=> {
 	}
 	return rows
 }
-const xmlLine = (x1, y1, x2, y2)=> {
-	return xml("line", { x1: x1, y1: y1, x2: x2, y2: y2 })
+const xmlLine = (p1, p2)=> {
+	return xml("line", { x1: p1.x, y1: p1.y, x2: p2.x, y2: p2.y })
 }
 
-const xmlText = (x, y, text, R, a)=> {
-	return xml("text", { 
-		x: (x + R*Math.sin(+a*Math.PI/180)).toFixed(4), 
-		y: (y + R*Math.cos(-a*Math.PI/180)).toFixed(4)
-	}, text)
+const polarToCartesian = (c, r, degrees)=> {
+  	const rad = (degrees-90) * Math.PI / 180.0
+  	return {
+    	x: c.x + (r * Math.cos(rad)),
+    	y: c.y + (r * Math.sin(rad))
+  	}
+}
+
+const xmlText = (c, text, R, A)=> {
+	const r = R || 0
+	const a = A || 0
+	return xml("text", polarToCartesian(c, r, a), text)
+}
+
+const xmlPathArc = (c, r, startAngle, angle) => {
+	if (!angle)
+		return ""
+    const a1 = polarToCartesian(c, r, startAngle + angle)
+    const a2 = polarToCartesian(c, r, startAngle)
+    const largeArcFlag = angle <= 180 ? "0" : "1"
+	return xml("path", { d: [
+        "M", a1.x, a1.y, 
+        "A", r, r, 0, largeArcFlag, 0, a2.x, a2.y
+    ].join(" ") })
 }
 
 const S = 333
@@ -37,89 +56,78 @@ const M = 140
 const sin = (x)=> { return (M*Math.sin(x*Math.PI)).toFixed(4) }
 const cos = (y)=> { return (M*Math.cos(y*Math.PI)).toFixed(4) }
 
-const A = { x: -sin(1/7), y: +cos(1/7) }
-const B = { x: +sin(1/7), y: +cos(1/7) }
-const C = { x: +sin(3/7), y: +cos(3/7) }
-const D = { x: +sin(5/7), y: +cos(5/7) }
-const E = { x: +sin(7/7), y: +cos(7/7) }
-const F = { x: -sin(5/7), y: +cos(5/7) }
-const G = { x: -sin(3/7), y: +cos(3/7) }
-const H = { x: +sin(7/7), y: -cos(3/7) }
+const p1 = { x: -sin(1/7), y: +cos(1/7) }
+const p2 = { x: +sin(1/7), y: +cos(1/7) }
+const p3 = { x: +sin(3/7), y: +cos(3/7) }
+const p4 = { x: +sin(5/7), y: +cos(5/7) }
+const p5 = { x: +sin(7/7), y: +cos(7/7) }
+const p6 = { x: -sin(5/7), y: +cos(5/7) }
+const p7 = { x: -sin(3/7), y: +cos(3/7) }
+
+const p8 = { x:p7.x-100, y:p7.y } // p7 little bit left
+
+const q1 = { x:p5.x, y:-cos(3/7) }
+const q2 = { x:p1.x, y:p7.y }
+const q3 = { x:p6.x, y:p7.y }
+const q4 = { x:p6.x, y:q1.y }
+
+const r1 = { x:q1.x, y:p1.y }
+const r2 = { x:.75*p1.x, y:p1.y }
 
 const heptagon = [
-	xmlLine(A.x, A.y, B.x, B.y),
-	xmlLine(B.x, B.y, C.x, C.y),
-	xmlLine(C.x, C.y, D.x, D.y),
-	xmlLine(D.x, D.y, E.x, E.y),
-	xmlLine(E.x, E.y, F.x, F.y),
-	xmlLine(F.x, F.y, G.x, G.y),
-	xmlLine(G.x, G.y, A.x, A.y)
+	xmlLine(p1, p2),
+	xmlLine(p2, p3),
+	xmlLine(p3, p4),
+	xmlLine(p4, p5),
+	xmlLine(p5, p6),
+	xmlLine(p6, p7),
+	xmlLine(p7, p1)
 ]
 const up = [
-	xml("line", { x1: D.x, y1: D.y, x2: H.x, y2: H.y }),
-	xml("line", { x1: H.x, y1: H.y, x2: F.x, y2: F.y }),
+	xmlLine(p4, q1),
+	xmlLine(q1, p6)
 ]
 
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-  return {
-    x: centerX + (radius * Math.cos(angleInRadians)),
-    y: centerY + (radius * Math.sin(angleInRadians))
-  };
-}
-
-const arc = (x, y, radius, startAngle, endAngle) => {
-    var start = polarToCartesian(x, y, radius, endAngle);
-    var end = polarToCartesian(x, y, radius, startAngle);
-    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-    return [
-        "M", start.x, start.y, 
-        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-    ].join(" ")
-}
-
 const orto = [
-	xmlLine(A.x, A.y, A.x, G.y),
-	xmlLine(A.x, G.y, G.x-100, G.y),
-	xmlLine(F.x, G.y, F.x, F.y),
-	xmlLine(F.x, H.y, H.x, H.y),
-	xmlLine(H.x, H.y, H.x, A.y),
-	xml("path", { d: arc(H.x, H.y, 40, 270, 270 + 1*180/7) }), // arc A
-	xml("path", { d: arc(G.x, G.y, 30,  90,  90 + 2*180/7) }), // arc B
-	xml("path", { d: arc(G.x, G.y, 20, 270, 270 + 4*180/7) })  // arc C
+	xmlLine(p1, q2),
+	xmlLine(q2, p8),
+	xmlLine(q3, p6),
+	xmlLine(q4, q1),
+	xmlLine(q1, r1),
+	xmlPathArc(q1, 40, 270, 180*1/7), // arc A
+	xmlPathArc(p7, 30,  90, 180*2/7), // arc B
+	xmlPathArc(p7, 20, 270, 180*4/7)  // arc C
 ]
 
 const texts = [
-	xmlText(H.x, H.y, "A", 50, -90 - 1*180/14),
-	xmlText(G.x, G.y, "B", 40,  90 - 2*180/14),
-	xmlText(G.x, G.y, "C", 30, 270 - 4*180/14),
+	xmlText(q1, "A", 50, 270 + 1*180/14),
+	xmlText(p7, "B", 40,  90 + 2*180/14),
+	xmlText(p7, "C", 30, 270 + 4*180/14),
 
-
-	xmlText(A.x, A.y, "p1", 12, 180 - 4*360/7),
-	xmlText(B.x, B.y, "p2", 12, 180 - 3*360/7),
-	xmlText(C.x, C.y, "p3", 12, 180 - 2*360/7),
-	xmlText(D.x, D.y, "p4", 12, 180 - 1*360/7),
-	xmlText(E.x, E.y, "p5", 12, 180),
-	xmlText(F.x, F.y, "p6", 12, 180 + 1*360/7),
-	xmlText(G.x, G.y, "p7", 15, -30),
-	xmlText(H.x, H.y, "q1", 12, 180),
-	xmlText(A.x, G.y, "q2", 12, 125),
-	xmlText(F.x, G.y, "q3", 15, 125),
-	xmlText(F.x, H.y, "q4", 15, 45),
-	xmlText(H.x, A.y, "r1", 15, 0),
-	xmlText(3*A.x/4, A.y, "r2", 15, 0),
+	xmlText(p1, "p1", 18, 4*360/7),
+	xmlText(p2, "p2", 18, 3*360/7),
+	xmlText(p3, "p3", 18, 2*360/7),
+	xmlText(p4, "p4", 18, 1*360/7),
+	xmlText(p5, "p5", 18),
+	xmlText(p6, "p6", 18, -1*360/7),
+	xmlText(p7, "p7", 18, 225),
+	xmlText(q1, "q1", 18), // at north
+	xmlText(q2, "q2", 18,  45),
+	xmlText(q3, "q3", 18,  45),
+	xmlText(q4, "q4", 18, 135),
+	xmlText(r1, "r1", 18, 180),
+	xmlText(r2, "r2", 18, 180),
 ]
 
-
 const diagonal = [
-	xmlLine(H.x, H.y, 3*A.x/4, A.y),
+	xmlLine(q1, r2),
 ]
 
 const groups = [
 	xml("g", { stroke:"#08f", "stroke-width":5 }, heptagon),
 	xml("g", { stroke:"#0a6", "stroke-width":5 }, up),
 	xml("g", { stroke:"#888", "stroke-width":1, fill:"none" }, orto),
-	xml("g", { stroke:"none", fill:"#888", "dominant-baseline":"middle", "text-anchor":"middle" }, texts),
+	xml("g", { "font-family":"Verdana", "font-size":16, stroke:"none", fill:"#888", "dominant-baseline":"middle", "text-anchor":"middle" }, texts),
 	xml("g", { stroke:"#f80", "stroke-width":5 }, diagonal),
 ]
 
