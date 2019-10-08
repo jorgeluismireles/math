8868de16c0da89e3a39cff25a21abd2dd5c89241
Jorge Mireles <jorgeluismireles@gmail.com>
2019-10-08 18:11:20 -0500

    wiki-models octagon

---
+++ meccano/svg.js
@@ -0,0 +1,44 @@
+const SVG = function()
+{
+	const SVG = 'http://www.w3.org/2000/svg'
+
+	const create = (elem)=> {
+		return document.createElementNS(SVG, elem);
+	}
+
+	this.svg = (width, height, obj)=> {
+	    var svg = create('svg')
+		svg.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+		svg.setAttribute("width", width)
+		svg.setAttribute("height", height)
+	    for (prop in obj)
+	        svg.setAttribute(prop, obj[prop])  
+	    return svg
+	}
+
+	this.g = (obj)=> {
+	    var g = create('g')
+	    for(prop in obj)
+	        g.setAttribute(prop, obj[prop])  
+	    return g
+	}
+
+	this.line = (obj)=> {
+	    var e = create('line')
+	    for (prop in obj)
+	        e.setAttribute(prop, obj[prop])  
+	    return e
+	}
+
+	this.path = (d, transform, obj)=> {
+	    var e = create('path')
+	    if (d) e.setAttribute("d", d);
+	    if (transform) e.setAttribute("transform", transform);
+	    if (obj) {
+		    for (prop in obj)
+		        e.setAttribute(prop, obj[prop])  
+	    }
+	    return e
+	}
+}
+
