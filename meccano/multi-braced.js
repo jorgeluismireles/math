18f3ee13180cfc9e501cb74808f55ebfd89d0bcc
Jorge Mireles <jorgeluismireles@gmail.com>
2019-10-21 17:06:47 -0500

    pentagon.html math page

--- meccano/multi-braced.js
+++ meccano/multi-braced.js
@@ -11,7 +11,254 @@ const MultiBraced = function(rtr, svgParams)
 
 const objects = 
 {
-	tetradecagon: (p, name)=> {
+	triangle: (p, name)=> {
+		const S = 30
+		const extremes = true
+		const G = new Polygon(p, name, S, {
+			8: "#08f"
+		}, false, extremes)
+		const C = (8/2)*S / Math.sin(Math.PI/3)
+		G.rods(3, (i, rod)=> {
+			rod(8, rtr(-120*i, C, 150))
+		})
+		G.end()
+	},
+
+	square_4: (p, name)=> {
+		const S = 25
+		const skip = true
+		const G = new Polygon(p, name, S, {
+			 8: "#08f",
+			10: "#f20"
+		}, skip)
+		const C = 8*S*Math.sqrt(2)/2 // circuncenter
+		const acos4_5 = 180*Math.acos(4/5)/Math.PI
+
+		G.rods(4, (i, rod)=> {
+			rod(8, rtr(-90*i, C, -90-45))
+			if (i==0)
+				rod(10, rtr(-90*i, C, -90-45-acos4_5))
+		})
+		G.end()
+	},
+
+	pentagon_12_a: (p, name)=> {
+		const S = 18
+		const N = 12
+		const G = new Polygon(p, name, S, {
+			 9: "#08f",
+			11: "#0d2",
+			12: "#f20"
+		})
+		const C = N*S/(2*Math.sin(Math.PI/5))
+
+		const sin18 = Math.sin(Math.PI/10)
+		const cos18 = Math.cos(Math.PI/10)
+		const a1 = -90-36
+		const p1 = { x:N*S*sin18, y:N*S*cos18 }
+		const p2 = { x:3*S,       y:0         }
+		const p3 = { x:4*S*sin18, y:4*S*cos18 }
+		const a3 = 180*Math.atan((p1.x + p2.x + p3.x)/(p1.y - p3.y))/Math.PI
+
+		G.rods(5, (i, rod)=> {
+			rod(12, rtr(-72*i, C, a1))
+			if (i==1) {
+				rod(11, rtr(-72*i, C, -90-18-a3))
+			}
+			if (i==4) {
+				const t = rtr(-72*i, C, a1-36)
+				rod( 9, `${t}`)
+				rod(12, `${t} translate(0,${S*9}) rotate(-72)`)
+				rod(11, `${t} translate(0,${S*9}) rotate(${-90+a3})`)
+			}
+		})
+		G.end()
+	},
+
+	pentagon_12_z_wrong: (p, name)=> {
+		const S = 16
+		const G = new Polygon(p, name, S, {
+			 9: "#08f",
+			11: "#0f4",
+			12: "#f20"
+		})
+		const C = 12*S/(2*Math.sin(Math.PI/5))
+
+		G.rods(5, (i, rod)=> {
+			const rt1 = `rotate(${-72*i}) translate(0 ${C})`
+			const r2 = -90-36
+			rod(12, `${rt1} rotate(${r2})`)
+			if (i==4) {
+				const rt2 = (m)=> `rotate(${-m*36}) translate(${m*4*S},0) rotate(${m*(90+72)})`
+				rod( 9, `${rt1} ${rt2(-1)}`)
+				rod( 9, `${rt1} ${rt2(+1)}`)
+				rod(11, `${rt1} ${rt2(-1)} translate(0, ${9*S}) rotate(-64.4)`)
+				rod(11, `${rt1} ${rt2(+1)} translate(0, ${9*S}) rotate(+64.4)`)
+			}
+		})
+		G.end()
+	},
+
+	hexagon_1: (p, name)=> {
+		const S = 70
+		const skip = true
+		const G = new Polygon(p, name, S, {
+			2: "#08f",
+			4: "#f20"
+		}, skip)
+		const C = 2*S
+		G.rods(6, (i, rod)=> {
+			const t1 = `rotate(${-60*i}) translate(0 ${C})`
+			rod(2, `${t1} rotate(120)`)
+			if (i==0 || i==1) {
+				rod(4, `${t1} rotate(180)`)
+			}
+		})
+		G.end()
+	},
+
+	heptagon_3: (p, name)=> {
+		const S = 25
+		const skip = true
+		const G = new Polygon(p, name, S, {
+			6: "#08f",
+			8: "#f20"
+		}, skip)
+		const C = 6*S / (2*Math.sin(Math.PI/7))
+		const R = 180*(Math.PI/7)/Math.PI
+		const a1 = +90+3*R
+		const a4 = 180*Math.atan(1/8)/Math.PI
+		G.rods(7, (i, rod)=> {
+			const t1 = `rotate(${-2*R*i}) translate(0 ${C})`
+			rod(6, `${t1} rotate(${90+R})`)
+			if (i==0 || i==2) {
+				const t2 = `translate(0 ${6*S}) rotate(${-2*R})`
+				rod(6, `${t1} rotate(${a1})`)
+				rod(6, `${t1} rotate(${a1}) ${t2}`)
+				rod(8, `${t1} rotate(${a1}) ${t2} rotate(${(9*R/2) + a4})`)
+				rod(8, `${t1} rotate(${a1}) ${t2} rotate(${(9*R/2) - a4})`)
+			}
+		})
+		G.end()
+	},
+
+	octagon_4: (p, name)=> {
+		const S = 30
+		const G = new Polygon(p, name, S, {
+			4: "#08f",
+			5: "#0f4",
+			6: "#f20"
+		})
+		const C = 4*S*Math.sqrt(4 + 2*Math.sqrt(2))/2 // circumradius
+		const r4 = [], r5 = [], r6 = []
+		const acos4_5 = 180*Math.acos(4/5)/Math.PI
+		const asin2_6 = 180*Math.asin(2/6)/Math.PI
+
+		G.rods(8, (i, rod)=> {
+			const rt1 = `rotate(${-45*i}) translate(0 ${C})`
+			const r2 = -90-22.5
+			rod(4, `${rt1} rotate(${r2})`)
+			if (i==0) { // north
+				rod(4, `${rt1} rotate(${r2 - 90})`)
+				rod(5, `${rt1} rotate(${r2 - acos4_5})`)
+			}
+			if (i==1) { // north-east
+				rod(4, `${rt1} rotate(${r2 - 45})`)
+			}
+			if (i==3) {
+				const t3a = `${rt1} rotate(${r2 - 90})`
+				const t3b = `translate(0 ${4*S}) rotate(${180 - acos4_5})`
+				const t3c = `translate(0 ${4*S}) rotate(${180 - 45 - asin2_6})`
+				rod(4, `${t3a}`)
+				rod(5, `${t3a} ${t3b}`)
+				rod(6, `${t3a} ${t3c}`)
+			}
+			if (i==6) {
+				const t6a = `${rt1} rotate(${r2 - 45})`
+				const t6b = `translate(0 ${4*S}) rotate(${180 + acos4_5})`
+				const t6c = `translate(0 ${4*S}) rotate(${180 + 45 + asin2_6})`
+				rod(4, `${t6a}`)
+				rod(5, `${t6a} ${t6b}`)
+				rod(6, `${t6a} ${t6c}`)
+			}
+		})
+		G.end()
+	},
+
+	decagon_12: (p, name)=> {
+		const S = 9
+		const G = new Polygon(p, name, S, {
+			 9: "#08f",
+			11: "#0d2",
+			12: "#f20"
+		})
+		const C = 12*S/(2*Math.sin(Math.PI/10))
+
+		const a1 = -90-18
+		const sin18 = Math.sin(Math.PI/10)
+		const cos18 = Math.cos(Math.PI/10)
+		const p1 = { x:12*S*sin18, y:12*S*cos18 }
+		const p2 = { x: 3*S,       y: 0 }
+		const p3 = { x: 4*S*sin18, y: 4*S*cos18 }
+		const a3 = 180*Math.atan((p1.x + p2.x + p3.x)/(p1.y - p3.y))/Math.PI
+
+		G.rods(10, (i, rod)=> {
+			rod(12, rtr(-36*i, C, a1))
+			if (i==0 || i==2 || i==6) {
+				const t2 = rtr(-36*i, C, 2*a1)
+				rod(12, t2)
+				rod(12, `${t2} translate(0 ${12*S}) rotate(-36)`)
+				rod(12, `${t2} translate(0 ${12*S}) rotate(+72)`)
+			}
+			if (i==3 || i==7) {
+				rod( 9, `rotate(${-36*i})`)
+				rod(12, rtr(-36*i, 9*S, +72))
+				rod(12, rtr(-36*i, 9*S, -72))
+				rod(11, rtr(-36*i, 9*S, +90-a3))
+				rod(11, rtr(-36*i, 9*S, -90+a3))
+				rod(11, rtr(-36*i,   C, +90+a3))
+				rod(11, rtr(-36*i,   C, -90-a3))
+			}
+		})
+		G.end()
+	},
+	
+	dodecagon_3: (p, name)=> {
+		const S = 15
+		const skip = true
+		const N = 6
+		const G = new Polygon(p, name, S, {
+			 6: "#08f",
+			 8: "#0f2",
+			10: "#f80",
+		    12: "#f20",
+		}, skip)
+		const C = N*S / (2*Math.sin(Math.PI/12))
+		const R = 180*(Math.PI/12)/Math.PI
+		const TR = (a)=> `translate(0 ${N*S}) rotate(${a})`
+		G.rods(12, (i, rod)=> {
+			rod(6, rtr(-2*R*i, C, 90+R)) // perimeter
+			if (i==2 || i==4 || i==6 || i==8 || i==10) {
+				const t0 = rtr(-2*R*i, C, 90+5*R)
+				rod(6, `${t0}`)
+				rod(6, `${t0} ${TR(90)}`)
+				if (i!=10) rod(6, `${t0} ${TR(90)} ${TR(90)}`)
+				if (i==6 || i==8)
+					rod(12, `${t0} ${TR(90)} ${TR(-120)}`)
+			}
+			if (i==11) {
+				const t0 = rtr(-2*R*i, C, 90+7*R)
+				rod( 8, `${t0}`)
+				rod( 6, `${t0} ${TR(+30)}`)
+				rod( 6, `${t0} ${TR(+30+90)}`)
+				const acos4_5 = 180*Math.acos(4/5)/Math.PI
+				rod(10, `${t0} translate(0 ${8*S}) rotate(${180+acos4_5})`)
+			}
+		})
+		G.end()
+	},
+
+	tetradecagon_3: (p, name)=> {
 		const S = 12
 		const skip = true
 		const G = new Polygon(p, name, S, {
@@ -71,219 +318,8 @@ const objects =
 		G.end()
 	},
 
-	triangle: (p, name)=> {
-		const S = 60
-		const extremes = true
-		const G = new Polygon(p, name, S, {
-			4: "#08f"
-		}, false, extremes)
-		const C = (4/2)*S / Math.sin(Math.PI/3)
-		G.rods(3, (i, rod)=> {
-			rod(4, rtr(-120*i, C, 150))
-		})
-		G.end()
-	},
 
-	square: (p, name)=> {
-		const S = 25
-		const skip = true
-		const G = new Polygon(p, name, S, {
-			 8: "#08f",
-			10: "#f20"
-		}, skip)
-		const C = 8*S*Math.sqrt(2)/2 // circuncenter
-		const acos4_5 = 180*Math.acos(4/5)/Math.PI
-
-		G.rods(4, (i, rod)=> {
-			rod(8, rtr(-90*i, C, -90-45))
-			if (i==0)
-				rod(10, rtr(-90*i, C, -90-45-acos4_5))
-		})
-		G.end()
-	},
-
-	pentagon1: (p, name)=> {
-		const S = 18
-		const G = new Polygon(p, name, S, {
-			 9: "#08f",
-			11: "#0d2",
-			12: "#f20"
-		})
-		const C = 12*S/(2*Math.sin(Math.PI/5))
-
-		const sin18 = Math.sin(Math.PI/10)
-		const cos18 = Math.cos(Math.PI/10)
-		const a1 = -90-36
-
-		const p1 = { x:12*S*sin18, y:12*S*cos18 }
-		const p2 = { x: 3*S,       y: 0 }
-		const p3 = { x: 4*S*sin18, y: 4*S*cos18 }
-		const a3 = 180*Math.atan((p1.x + p2.x + p3.x)/(p1.y - p3.y))/Math.PI
-
-		G.rods(5, (i, rod)=> {
-			rod(12, rtr(-72*i, C, a1))
-			if (i==1) {
-				rod(11, rtr(-72*i, C, -90-18-a3))
-			}
-			if (i==4) {
-				const t = rtr(-72*i, C, a1-36)
-				rod( 9, `${t}`)
-				rod(12, `${t} translate(0,${S*9}) rotate(-72)`)
-				rod(11, `${t} translate(0,${S*9}) rotate(${-90+a3})`)
-			}
-		})
-		G.end()
-	},
-
-	pentagon2: (p, name)=> {
-		const S = 16
-		const G = new Polygon(p, name, S, {
-			 9: "#08f",
-			11: "#0f4",
-			12: "#f20"
-		})
-		const C = 12*S/(2*Math.sin(Math.PI/5))
-
-		G.rods(5, (i, rod)=> {
-			const rt1 = `rotate(${-72*i}) translate(0 ${C})`
-			const r2 = -90-36
-			rod(12, `${rt1} rotate(${r2})`)
-			if (i==4) {
-				const rt2 = (m)=> `rotate(${-m*36}) translate(${m*4*S},0) rotate(${m*(90+72)})`
-				rod( 9, `${rt1} ${rt2(-1)}`)
-				rod( 9, `${rt1} ${rt2(+1)}`)
-				rod(11, `${rt1} ${rt2(-1)} translate(0, ${9*S}) rotate(-64.4)`)
-				rod(11, `${rt1} ${rt2(+1)} translate(0, ${9*S}) rotate(+64.4)`)
-			}
-		})
-		G.end()
-	},
-
-	hexagon: (p, name)=> {
-		const S = 70
-		const skip = true
-		const G = new Polygon(p, name, S, {
-			2: "#08f",
-			4: "#f20"
-		}, skip)
-		const C = 2*S
-		G.rods(6, (i, rod)=> {
-			const t1 = `rotate(${-60*i}) translate(0 ${C})`
-			rod(2, `${t1} rotate(120)`)
-			if (i==0 || i==1) {
-				rod(4, `${t1} rotate(180)`)
-			}
-		})
-		G.end()
-	},
-
-	heptagon: (p, name)=> {
-		const S = 25
-		const skip = true
-		const G = new Polygon(p, name, S, {
-			6: "#08f",
-			8: "#f20"
-		}, skip)
-		const C = 6*S / (2*Math.sin(Math.PI/7))
-		const R = 180*(Math.PI/7)/Math.PI
-		const a1 = +90+3*R
-		const a4 = 180*Math.atan(1/8)/Math.PI
-		G.rods(7, (i, rod)=> {
-			const t1 = `rotate(${-2*R*i}) translate(0 ${C})`
-			rod(6, `${t1} rotate(${90+R})`)
-			if (i==0 || i==1 || i==2) {
-				const t2 = `translate(0 ${6*S}) rotate(${-2*R})`
-				rod(6, `${t1} rotate(${a1})`)
-				rod(6, `${t1} rotate(${a1}) ${t2}`)
-				rod(8, `${t1} rotate(${a1}) ${t2} rotate(${(9*R/2) + a4})`)
-				rod(8, `${t1} rotate(${a1}) ${t2} rotate(${(9*R/2) - a4})`)
-			}
-		})
-		G.end()
-	},
-
-	octagon1: (p, name)=> {
-		const S = 30
-		const G = new Polygon(p, name, S, {
-			4: "#08f",
-			5: "#0f4",
-			6: "#f20"
-		})
-		const C = 4*S*Math.sqrt(4 + 2*Math.sqrt(2))/2 // circumradius
-		const r4 = [], r5 = [], r6 = []
-		const acos4_5 = 180*Math.acos(4/5)/Math.PI
-		const asin2_6 = 180*Math.asin(2/6)/Math.PI
-
-		G.rods(8, (i, rod)=> {
-			const rt1 = `rotate(${-45*i}) translate(0 ${C})`
-			const r2 = -90-22.5
-			rod(4, `${rt1} rotate(${r2})`)
-			if (i==0) { // north
-				rod(4, `${rt1} rotate(${r2 - 90})`)
-				rod(5, `${rt1} rotate(${r2 - acos4_5})`)
-			}
-			if (i==1) { // north-east
-				rod(4, `${rt1} rotate(${r2 - 45})`)
-			}
-			if (i==3) {
-				const t3a = `${rt1} rotate(${r2 - 90})`
-				const t3b = `translate(0 ${4*S}) rotate(${180 - acos4_5})`
-				const t3c = `translate(0 ${4*S}) rotate(${180 - 45 - asin2_6})`
-				rod(4, `${t3a}`)
-				rod(5, `${t3a} ${t3b}`)
-				rod(6, `${t3a} ${t3c}`)
-			}
-			if (i==6) {
-				const t6a = `${rt1} rotate(${r2 - 45})`
-				const t6b = `translate(0 ${4*S}) rotate(${180 + acos4_5})`
-				const t6c = `translate(0 ${4*S}) rotate(${180 + 45 + asin2_6})`
-				rod(4, `${t6a}`)
-				rod(5, `${t6a} ${t6b}`)
-				rod(6, `${t6a} ${t6c}`)
-			}
-		})
-		G.end()
-	},
-	
-	decagon: (p, name)=> {
-		const S = 9
-		const G = new Polygon(p, name, S, {
-			 9: "#08f",
-			11: "#0d2",
-			12: "#f20"
-		})
-		const C = 12*S/(2*Math.sin(Math.PI/10))
-
-		const a1 = -90-18
-		const sin18 = Math.sin(Math.PI/10)
-		const cos18 = Math.cos(Math.PI/10)
-		const p1 = { x:12*S*sin18, y:12*S*cos18 }
-		const p2 = { x: 3*S,       y: 0 }
-		const p3 = { x: 4*S*sin18, y: 4*S*cos18 }
-		const a3 = 180*Math.atan((p1.x + p2.x + p3.x)/(p1.y - p3.y))/Math.PI
-
-		G.rods(10, (i, rod)=> {
-			rod(12, rtr(-36*i, C, a1))
-			if (i==0 || i==2 || i==6) {
-				const t2 = rtr(-36*i, C, 2*a1)
-				rod(12, t2)
-				rod(12, `${t2} translate(0 ${12*S}) rotate(-36)`)
-				rod(12, `${t2} translate(0 ${12*S}) rotate(+72)`)
-			}
-			if (i==3 || i==7) {
-				rod( 9, `rotate(${-36*i})`)
-				rod(12, rtr(-36*i, 9*S, +72))
-				rod(12, rtr(-36*i, 9*S, -72))
-				rod(11, rtr(-36*i, 9*S, +90-a3))
-				rod(11, rtr(-36*i, 9*S, -90+a3))
-				rod(11, rtr(-36*i,   C, +90+a3))
-				rod(11, rtr(-36*i,   C, -90-a3))
-			}
-		})
-		G.end()
-	},
-
-	dodecagon: (p, name)=> {
+	dodecagon_2: (p, name)=> {
 		const S = 20
 		const skip = true
 		const G = new Polygon(p, name, S, {
--- meccano/node/pentagon.js
+++ meccano/node/pentagon.js
@@ -1,89 +1,57 @@
 const sqrt5 = (int, rad, den)=> (int + rad*Math.sqrt(5)) / den
 
-const Pentagon1 = function()
+function MeccanoPentagonI(sols)
 {
-	this.R = (a,b,c)=> (a*b - a*c + c*b) / 2
-
-	this.Z = (a,b,c)=> a*a + b*b + c*c - (a*b + a*c + c*b) / 2
-
-	const self = this
-
-	this.solutions = (aMax, message, list)=> { // b > a
-		const sols = []
-		const now = +new Date()
-		for (let a=1; a < aMax; a++) {
-			for (let b=1; b <= aMax; b++)
+	this.find = (max)=> {
+		for (let a=1; a < max; a++)
+			for (let b=1; b <= max; b++)
 				for (let c=0; c <= a; c++)
-					test(list, sols, a, b, c)
-		}
-		message(`Tested ${aMax} : ${+new Date() - now}ms`)
+					if ((a + c)*b == a*c)
+						mZero(a, b, c)
 	}
-	
-	const test = (list, sols, a, b, c)=> {
-		if (self.R(a, b, c) != 0)
-			return
-		const d = Math.sqrt(self.Z(a, b, c))
-		if (d > 0 && d % 1 === 0) {
-			const s = add(sols, a, b, c, d)
-			if (s)
-				list(s)
-			//console.log(`a=${a} b=${b} c=${c} d=${d}`)
-		}
-
+	const mZero = (a, b, c)=> {
+		const d = Math.sqrt(a*a + b*b + c*c - a*c)
+		if (d > 0 && d % 1 === 0)
+			dInteger(a, b, c, d)
 	}
-	const add = (sols, a, b, c, d) => {
+	const dInteger = (a, b, c, d) => {
 		for (let i=0; i < sols.length; i++) {
-			const sol = sols[i]
-			if (a % sol.a == 0) {
-				const f = a / sol.a
-				const bS = (b % sol.b == 0) && b / sol.b == f
-				const cS = (c % sol.c == 0) && c / sol.c == f
-				const dS = (d % sol.d == 0) && d / sol.d == f
+			const s = sols[i]
+			if (a % s.a == 0) {
+				const f = a / s.a
+				const bS = (b % s.b == 0) && b / s.b == f
+				const cS = (c % s.c == 0) && c / s.c == f
+				const dS = (d % s.d == 0) && d / s.d == f
 				if (bS && cS && dS)
-					return null
+					return // scaled solution already
 			}
 		}
-		sols.push( { a:a, b:b, c:c, d:d })
-		return `n=${sols.length} a=${a} b=${b} c=${c} d=${d}`
+		sols.push({ a:a, b:b, c:c, d:d }) // solution!
 	}
 }
 
 
-
-const Pentagon2 = function()
+//this.R = (a,b,c,d) => ( (a - b)*(c - d) + a*b - c*d ) / 2
+// const self = this
+function MeccanoPentagonII(sols)
 {
-
-	this.R = (a,b,c,d) => ( (a - b)*(c - d) + a*b - c*d ) / 2
-
-	this.Z = (a,b,c,d) => a*a + b*b + c*c + d*d - ( (a + b)*(c+d) + a*b + c*d ) / 2
-
-	const self = this
-
-	this.solutions = (aMax, message, list)	=> {
-		const sols = []
-		const now = +new Date()
-		for (let a=1; a < aMax; a++) {
+	const Z = (a,b,c,d) => a*a + b*b + c*c + d*d - ( (a + b)*(c+d) + a*b + c*d ) / 2
+	this.find = (max) => {
+		for (let a=1; a < max; a++) {
 			for (let b=1; b < a; b++)
 				for (let c=1; c < a; c++)
 					for (let d=1; d < a; d++)
-						test(list, sols, a, b, c, d)
-		}
-		message(`Tested ${aMax} : ${+new Date() - now}ms`)
-	}
-
-	const test = (list, sols, a, b, c, d)=> {
-		if (self.R(a,b,c,d) != 0)
-			return
-		const e2 = self.Z(a,b,c,d)
-		const e = Math.sqrt(e2)
-		if (e > 0 && e % 1 === 0) {
-			const s = add(sols, a, b, c, d, e)
-			if (s)
-				list(s)
+						if (((a - b)*(c - b) + a*b) == c*d)
+							mZero(a, b, c, d)
 		}
 	}
-
-	const add = (sols, a, b, c, d, e)=> {
+	const mZero = (a, b, c, d)=> {
+		//const e = Math.sqrt(Z(a,b,c,d))
+		const e = Math.sqrt(a*a + b*b + c*c + d*d - a*d - b*c - c*d)
+		if (e > 0 && e % 1 === 0)
+			eInteger(a, b, c, d, e)
+	}
+	const eInteger = (a, b, c, d, e)=> {
 		for (let i=0; i < sols.length; i++) {
 			const sol = sols[i]
 			if (a % sol.a == 0) {
@@ -93,36 +61,9 @@ const Pentagon2 = function()
 				const dS = (d % sol.d == 0) && d / sol.d == f
 				const eS = (e % sol.e == 0) && e / sol.e == f
 				if (bS && cS && dS && eS)
-					return null
+					return // scaled solution already
 			}
 		}
-		sols.push( { a:a, b:b, c:c, d:d, e:e })
-		const n = sols.length
-		console.log(`n=${n} e=${Math.sqrt(a*a + b*b + c*c + d*d - a*d - b*c - c*d) }`)
-		return `n=${n} a=${a} b=${b} c=${c} d=${d} e=${e}`
+		sols.push( { a:a, b:b, c:c, d:d, e:e }) // solution
 	}
-
-	/*
-	const test1 = (a,b,c,d)=>
-		a*a 
-		+ (b + c)*(b + c) * sqrt5( 3, -1, 8) // cosA*cosA //
-		+ d*d             * sqrt5( 3,  1, 8) // cosB*cosB // 
-		+ 2*a*(b + c)     * sqrt5(-1,  1, 4) // cosA      // 
-		- 2*a*d           * sqrt5( 1,  1, 4) // cosB      // 
-		- 2*(b + c) * d   * sqrt5( 1,  0, 4) // cosA*cosB //  
-
-		+ (c - b)*(c - b) * sqrt5( 5,  1, 8) // sinA*sinA // 
-		+ d*d             * sqrt5( 5, -1, 8) // sinB*sinB 
-		- 2*(c - b)*d     * sqrt5( 0,  1, 4) // sinA*sinB //
-
-
-	const test2 = (a,b,c,d) => //( 8*a*a + 3*(b+c)*(b+c) + 3*d*d - 4*a*(b+c) - 4*a*d - 4*(b+c)*d + 5*(c-b)*(c-b) + 5*d*d ) / 8
-                  // a*a + b*b + c*c + d*d - ( a*(b+c+d) + b*(c+d) + c*d  )/2
-				  a*a + b*b + c*c + d*d - ( (a + b)*(c+d) + a*b + c*d ) / 2
-
-	const test3 = (a,b,c,d)=> //-(b+c)*(b+c) + d*d + 4*a*(b+c) - 4*a*d + (c-b)*(c-b) - d*d - 4*(c-b)*d
-	              //( a*(b + c - d) - b*(c - d) - c*d  ) / 2
-	              ( (a - b)*(c - d) + a*b - c*d ) / 2
-	*/
 }
-
